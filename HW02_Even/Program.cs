191225c1be057b096b9c558c286c09bf26a8cf66
German <nemets80@mail.ru>
2022-12-04 02:35:52 +0300

    Add Even

---
+++ HW02_Even/Program.cs
@@ -0,0 +1,33 @@
+﻿int a = 4;
+int b = -3;
+int c = 7;
+
+Console.Write("Число a - ");
+    if (a % 2 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+
+Console.Write("Число b - ");
+    if (b % 2 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+
+Console.Write("Число c - ");
+    if (c % 2 == 0)
+    {
+         Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
